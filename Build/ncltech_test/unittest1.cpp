e1c5a5ac34d24693bc49135bc9af90ee811121a6
Dan Nixon <dan@dan-nixon.com>
2016-12-05 19:14:03 +0000

    add unit test project

---
+++ Build/ncltech_test/unittest1.cpp
@@ -0,0 +1,19 @@
+#include <CppUnitTest.h>
+
+#include <ncltech/AABB.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ncltech_test
+{
+TEST_CLASS(UnitTest1)
+{
+public:
+  TEST_METHOD(TestMethod1)
+  {
+    // TODO
+    AABB a;
+    a.GetNumEdges();
+  }
+};
+}
